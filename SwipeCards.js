77ed1b62536b96411990f5ad0be7caa1325fa659
Tyler Buchea <tyler@buchea.com>
2016-08-12 12:03:11 -0700

    Fix broken React / ReactNative import statements

--- SwipeCards.js
+++ SwipeCards.js
@@ -1,7 +1,6 @@
 /* Gratefully copied from https://github.com/brentvatne/react-native-animated-demo-tinder */
-'use strict';
-
-import React, {Component} from 'react';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 import {
     StyleSheet,
